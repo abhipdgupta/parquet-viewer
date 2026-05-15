420e16615c819ce7361a2187d1ba18ebdd9720fe
Abhishek Pd. Gupta <abhipdgupt007@gmail.com>
2026-05-16 00:15:28 +0530

    github pages deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/parquet-viewer", // Set the base path for the application
   worker: {
     format: "es",
   },
